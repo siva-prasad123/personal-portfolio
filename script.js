8be1c1d9a483b419b962b3a9e1ebc4c3fd994aa9
siva-prasad123 <68691914+siva-prasad123@users.noreply.github.com>
2024-05-13 12:50:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ $(document).ready(function(){
 
     // Scroll to the message
     messageElement.scrollIntoView({ behavior: 'smooth' });
-    messageElement.textContent = "perlasivaprasad2@gmail.com"; 
+    messageElement.textContent2 = "perlasivaprasad2@gmail.com"; 
     // You can also display an alert message
-    alert("This is just for a demo purpose. You can contact me through this mails" + messageElement.textContent); // Uncomment this line if you want to display an alert
+    alert("This is just for a demo purpose. You can contact me through this mails" + messageElement.textContent2); // Uncomment this line if you want to display an alert
 });
 
     
